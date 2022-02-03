12999a88e1ee329572fef04b105fdecdc9e5f0a4
swetshah <swetshah1994@gmail.com>
2022-02-03 19:47:54 +0530

    #3-milestone: Added an API to fetch User stored queries

--- UserManagement/src/main/java/com/storage/service/QueryStorageService.java
+++ UserManagement/src/main/java/com/storage/service/QueryStorageService.java
@@ -1,5 +1,7 @@
 package com.storage.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,10 @@ public class QueryStorageService {
 
 	public UserQuery saveUserQueryDetails(UserQuery userQuery) {
 		return queryStorageRepository.save(userQuery);
+	}
+
+	public List<UserQuery> getUserQuery(String userName) {
+		return queryStorageRepository.findByUserName(userName);
 	}
 
 }
