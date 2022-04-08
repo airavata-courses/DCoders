eef845dbb49f157d4870b34b6138ce1e03025974
vinayakgadag <vinayaksgadag@gmail.com>
2022-04-08 02:32:29 -0400

    #30-milestone3: investigate the path-1

--- deployment_scripts/copy_files.sh
+++ deployment_scripts/copy_files.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 mkdir -p deploy-files
+echo "Current directory of the task"
+pwd
+echo "Begin copying the deployment scripts to current job's container from resource container"
 
 cp -rf git-ds/datasource_service/k8s_deployments/* deploy-files/
 
