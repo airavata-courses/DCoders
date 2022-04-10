048006092d8d23806b8a7828fa88fd3a89d0367d
vinayakgadag <vinayaksgadag@gmail.com>
2022-04-09 21:58:28 -0400

    #30-milestone3: update task script

--- deployment_scripts/copy_files.sh
+++ deployment_scripts/copy_files.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
-mkdir -p deploy-files
+mkdir -p deploy-files/datasource_service
+mkdir -p deploy-files/api_gateway
+#mkdir -p deploy-files/weather_ui
+mkdir -p deploy-files/UserManagement
 echo "Current directory of the task"
 pwd
 echo "Begin copying the deployment scripts to current job's container from resource container"
 
-cp -rf git-ds/datasource_service/k8s_deployments/* deploy-files/
+cp -rf git-ds/datasource_service/k8s_deployments/* deploy-files/datasource_service/
+cp -rf git-ds/api_gateway/k8s_deployments/* deploy-files/api_gateway/
+#cp -rf ../weather_ui/k8s_deployments/* deploy-files/weather_ui/
+cp -rf git-ds/UserManagement/k8s_deployments/* deploy-files/UserManagement/
 
 ls deploy-files/*
 pwd
